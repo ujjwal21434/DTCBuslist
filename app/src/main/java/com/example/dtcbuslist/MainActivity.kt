9e1e7554678ce3023f948633ad1b12cf9156f95d
Ujjwal Singhal <108088836+ujjwal21434@users.noreply.github.com>
2024-05-21 10:44:19 +0530

    improve

--- app/src/main/java/com/example/dtcbuslist/MainActivity.kt
+++ app/src/main/java/com/example/dtcbuslist/MainActivity.kt
@@ -1,32 +1,25 @@
 package com.example.dtcbuslist
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
 import android.widget.Button
-import android.widget.EditText
-import com.google.android.material.appbar.MaterialToolbar
+import androidx.appbar.app.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(findViewById(R.id.topAppBar))
 
-
-        val button1 = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener {
-            val intent = Intent(this, FirstActivity::class.java)
-            startActivity(intent)
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            startActivity(Intent(this, FirstActivity::class.java))
         }
 
-        val button2 = findViewById<Button>(R.id.button2)
-        button2.setOnClickListener {
-            val intent = Intent(this, LocationActivity::class.java)
-            startActivity(intent)
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            startActivity(Intent(this, LocationActivity::class.java))
         }
     }
 }

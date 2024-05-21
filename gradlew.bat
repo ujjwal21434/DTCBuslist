cb54546f91f57814a839565911899de882ae73d3
Ujjwal Singhal <108088836+ujjwal21434@users.noreply.github.com>
2024-05-21 10:45:37 +0530

    comment

--- gradlew.bat
+++ gradlew.bat
@@ -1,16 +1,4 @@
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
+@rem Copyright notice and license information.
 
 @if "%DEBUG%" == "" @echo off
 
@@ -23,8 +11,11 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
+@rem Set the directory of the current script
 set "DIRNAME=%~dp0"
 if "%DIRNAME%" == "" set "DIRNAME=."
+
+@rem Set the base name of the current script
 set "APP_BASE_NAME=%~n0"
 set "APP_HOME=%DIRNAME%"
 
@@ -71,8 +62,8 @@ set "CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar"
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
-:end
 @rem End local scope for the variables with windows NT shell
+:end
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail

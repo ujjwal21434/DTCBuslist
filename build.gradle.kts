def2aeac13d49d22a1ffbc0c68aa7cf08f8ba448
Ujjwal Singhal <108088836+ujjwal21434@users.noreply.github.com>
2024-05-21 10:44:34 +0530

    improve

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,31 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
+        classpath("com.chaquo.python:gradle:15.0.1")
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.chaquo.python") version "15.0.1" apply false
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }

ea710988d79e9110c0971b56678952c0c87f533b
Ujjwal Singhal <108088836+ujjwal21434@users.noreply.github.com>
2024-05-21 10:45:15 +0530

    improve

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,31 @@
+plugins {
+    id 'com.android.application' version '7.2.0' apply false
+    id 'com.github.dcendents.android-maven' version '2.1'
+    id 'org.jetbrains.kotlin.android' version '1.6.10'
+}
+
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
+        google()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
         mavenCentral()
+        google()
     }
 }
 
 rootProject.name = "DTCBuslist"
 include(":app")
- 
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}

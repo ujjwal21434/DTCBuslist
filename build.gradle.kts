6b33fb048671b08df9dd35ca5e5ed5d7efd9b87e
Ujjwal Singhal <108088836+ujjwal21434@users.noreply.github.com>
2024-05-21 10:45:45 +0530

    comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,31 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        jcenter()
+buildscript { // Configures the build script classpath and dependencies.
+    repositories { // Repository definitions for dependencies.
+        gradlePluginPortal() // Gradle Plugin Portal repository.
+        google() // Google's Maven repository.
+        jcenter() // JFrog's Bintray JCenter repository.
     }
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
-        classpath("com.chaquo.python:gradle:15.0.1")
-    }
-}
-
-plugins {
-    id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        jcenter()
+    dependencies { // Classpath dependencies for the build script.
+        classpath("com.android.tools.build:gradle:8.2.2") // Android Gradle plugin.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22") // Kotlin Gradle plugin.
+        classpath("com.chaquo.python:gradle:15.0.1") // Chaquopy Gradle plugin for Python support.
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+plugins { // Plugin block to configure plugins for the project.
+    id("com.android.application") apply false // Disables the Android application plugin.
+    id("org.jetbrains.kotlin.android") apply false // Disables the Kotlin Android plugin.
+}
+
+allprojects { // Configures repositories for all sub-projects.
+    repositories { // Repository definitions for sub-projects.
+        google() // Google's Maven repository.
+        jcenter() // JFrog's Bintray JCenter repository.
+    }
+}
+
+task clean(type: Delete) { // Defines a 'clean' task to delete the build directory.
+    delete rootProject.buildDir // Deletes the root project's build directory.
 }
